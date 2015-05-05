e2143aba08e1a9e454edfe1117fbd1211cfbeb7c
Bruno Casella <brunocasella@gmail.com>
2015-05-04 22:41:38 -0300

    Initiate project
    	- Uses lein to create a new clojure project

---
+++ git-experiment/test/git_experiment/core_test.clj
@@ -0,0 +1,7 @@
+(ns git-experiment.core-test
+  (:require [clojure.test :refer :all]
+            [git-experiment.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
